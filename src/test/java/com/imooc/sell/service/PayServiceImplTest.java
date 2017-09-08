dff1782c5d36221718f620d51757fbdbb66868ad
goSelf <345684180@qq.com>
2017-09-08 09:32:46 +0800

    删除警告

--- src/test/java/com/imooc/sell/service/PayServiceImplTest.java
+++ src/test/java/com/imooc/sell/service/PayServiceImplTest.java
@@ -1,18 +1,15 @@
 package com.imooc.sell.service;
 
-import com.imooc.sell.dto.OrderDTO;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import com.imooc.sell.dto.OrderDTO;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Slf4j
 public class PayServiceImplTest {
 
     @Autowired
