94d004666866debfc5247157bbc907d19fbbbbdd
goSelf <345684180@qq.com>
2017-09-11 11:55:39 +0800

    优化

--- src/main/java/com/imooc/sell/util/JsonUtil.java
+++ src/main/java/com/imooc/sell/util/JsonUtil.java
@@ -4,7 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class JsonUtil {
-    /** 对象格式化Json输出*/
+    /** 对象格式化JSon输出*/
     public static String toJson(Object object) {
         GsonBuilder gsonBuilder = new GsonBuilder();
         //逗号换行符
